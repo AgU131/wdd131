1d9d1e1b7501952316ab8f3261ce44ff5cc67997
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-04-12 23:19:35 -0300

    project development

---
+++ project/scripts/project.js
@@ -0,0 +1,8 @@
+// Dates
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+
+
+
+
+
